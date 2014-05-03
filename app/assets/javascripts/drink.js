4534e64971cf1bd5e9eabb4adf5271e0615d3925
AndrewNg <andrew@chesscademy.com>
2014-05-03 19:29:09 -0400

    fixed rounding, working on slider

--- app/assets/javascripts/drink.js
+++ app/assets/javascripts/drink.js
@@ -1,6 +1,17 @@
 // Handle the creation of a new drink
 
 var ready = function() {
+  $("#slider-range").slider({
+   orientation: "vertical",
+   range: true,
+   values: [ 0, 1000 ],
+   step: 1,
+   slide: function( event, ui ) {
+    $( ".hidden-number" ).val( ui.values[ 0 ] + " " + ui.values[ 1 ] );
+  }
+});
+  $( ".hidden-number" ).val($( "#slider-range" ).slider( "values", 0 ) + $( "#slider-range" ).slider( "values", 1 ) );
+
   $(".field-amount input:radio").click(function() {
     var selection = $(".field-amount input[type='radio']:checked").val();
     $("#solo_pic").toggleClass("blur_pic");
@@ -8,6 +19,7 @@ var ready = function() {
     $("#gatorade_pic").toggleClass("blur_pic");
     $("#water_pic").toggleClass("blur_pic");
     $("#" + selection + "_pic").toggleClass("blur_pic");
+    $("#slider-range").show();
   });
 }
 
