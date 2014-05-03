99eef6ee6842a39f77256eb6101cd121858df255
Francis Hinson <francis@orchive.com>
2014-05-03 11:27:25 -0400

    history barebone

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,4 +2,9 @@ class BaseController < ApplicationController
   def index
 
   end
+
+  def history
+    @user = current_user
+    @drinks = @user.drinks
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup', :edit => 'settings'}
   root 'base#index'
 
+  get 'history/' => 'base#history'
+
   resources :drinks, :except => [:index, :show, :edit]
 end
