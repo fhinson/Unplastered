dede9c3194f272a13712b2b0768b2ab2a7a05247
Francis Hinson <francis@orchive.com>
2014-05-04 13:32:27 -0400

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def calculate_bac(user)
     body_water_constant = user.sex == "male" ? 0.58 : 0.49
     weight = user.weight
-    drinks = (user.drinks.pluck(:amount).inject(:+))/0.6
+    drinks = (user.drinks.pluck(:amount).compact.inject(:+))/0.6
     metabolism_constant = 0.017
     drinking_period = (Time.now - user.drinks.first.created_at) / 3600
     bac = ((0.806 * drinks * 1.2)/(body_water_constant * weight)) - (metabolism_constant * drinking_period)
