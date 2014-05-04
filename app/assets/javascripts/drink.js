7646b2bd79dfe934e5d7014cef7fcf2a7c38f5c8
AndrewNg <andrew@chesscademy.com>
2014-05-03 20:26:38 -0400

    slider goes on top of the image

--- app/assets/javascripts/drink.js
+++ app/assets/javascripts/drink.js
@@ -10,8 +10,8 @@ var ready = function() {
     $( ".hidden-number" ).val( ui.values[ 0 ] + " " + ui.values[ 1 ] );
   }
 });
-  $( ".hidden-number" ).val($( "#slider-range" ).slider( "values", 0 ) + $( "#slider-range" ).slider( "values", 1 ) );
 
+  $( ".hidden-number" ).val($( "#slider-range" ).slider( "values", 0 ) + $( "#slider-range" ).slider( "values", 1 ) );
   $(".field-amount input:radio").click(function() {
     var selection = $(".field-amount input[type='radio']:checked").val();
     $("#solo_pic").toggleClass("blur_pic");
@@ -19,6 +19,19 @@ var ready = function() {
     $("#gatorade_pic").toggleClass("blur_pic");
     $("#water_pic").toggleClass("blur_pic");
     $("#" + selection + "_pic").toggleClass("blur_pic");
+    var height = ($("#" + selection + "_pic").height());
+
+    var rowPos = $("#" + selection + "_pic").position();
+    bottomTop = rowPos.top;
+    bottomLeft = rowPos.left;
+
+    $("#slider-range").css({
+      "height": height,
+      position: 'absolute',
+      top: bottomTop,
+      left: bottomLeft
+    });
+
     $("#slider-range").show();
   });
 }
