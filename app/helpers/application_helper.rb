77b5571d45db9933852298fe99d3b59bf531675f
Francis Hinson <francis@orchive.com>
2014-05-04 13:27:21 -0400

    cumulative bac

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
 module ApplicationHelper
   def calculate_bac(user)
-    drink = user.drinks.last
     body_water_constant = user.sex == "male" ? 0.58 : 0.49
     weight = user.weight
-    drinks = drink.amount/0.6
+    drinks = (user.drinks.pluck(:amount).inject(:+))/0.6
     metabolism_constant = 0.017
-    drinking_period = (Time.now - drink.created_at) / 3600
+    drinking_period = (Time.now - user.drinks.first.created_at) / 3600
     bac = ((0.806 * drinks * 1.2)/(body_water_constant * weight)) - (metabolism_constant * drinking_period)
     return bac > 0 ? bac : 0
   end
