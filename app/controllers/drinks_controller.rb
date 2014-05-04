f0d62166503a0a7e1dd0aa58eef96776dbe6138b
AndrewNg <andrew@chesscademy.com>
2014-05-04 13:25:21 -0400

    finished twilio

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,16 +1,30 @@
 class DrinksController < ApplicationController
   include ApplicationHelper
   before_action :authenticate_user!
+
+  require 'twilio-ruby'
+
   def new
     @drink = Drink.new
   end
 
   def create
     @drink = current_user.drinks.build(drink_params)
+    @user = current_user
+    account_sid = 'ACf790f48ac9a0c4a1cb5e5548945e0889'
+    auth_token = '8be80276be5dd74cf822b080068b1fd4'
+
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new account_sid, auth_token
 
     respond_to do |format|
       if @drink.save
         format.html { redirect_to "/"}
+        @client.account.messages.create(
+          :from => '+17328100203',
+          :to => @user.friendnumber,
+          :body => "I'm plastered as fuck"
+        )
       else
         format.html { render :new }
         format.json { render json: @drink.errors, status: :unprocessable_entity }
